9266935ba0ebf20035f5cad2f644d3888868297c
ANUSHAK-248 <anushakbhardwaj248@gmail.com>
2025-06-28 16:45:41 +0530

    removed extra comments

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -7,20 +7,3 @@ class ExpressError extends Error {
 }
 
 module.exports= ExpressError;
-
-
-// if(!listing.title){
-//         throw new ExpressError(400, "title is missing");
-//     }
-//     if(!listing.description){
-//         throw new ExpressError(400, "Description is missing");
-//     }
-//     if(!listing.price){
-//         throw new ExpressError(400, "price is missing");
-//     }
-//     if(!listing.location){
-//         throw new ExpressError(400, "location is missing");
-//     }
-//     if(!listing.country){
-//         throw new ExpressError(400, "country is missing");
-//     }
