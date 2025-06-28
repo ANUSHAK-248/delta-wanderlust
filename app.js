7b60e043f1987596c5ef40eb076f93f8c2fd4667
ANUSHAK-248 <anushakbhardwaj248@gmail.com>
2025-06-28 16:43:05 +0530

    removed extra loggings

--- app.js
+++ app.js
@@ -80,11 +80,6 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
-    if(req.user){
-        console.log("Current user  = ",req.user.username);        
-    }else{
-        console.log("Current user  = ",req.user);
-    }
     next();
 })
 
